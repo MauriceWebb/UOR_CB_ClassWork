2c0a1a7298316ae0d6b93da5427390c163364fb5
MauriceWebb <maurice.webb91@gmail.com>
2019-03-17 19:26:18 -0400

    fixed trivia. need to change question...

--- TriviaGame/assets/javascript/app.js
+++ TriviaGame/assets/javascript/app.js
@@ -3,29 +3,91 @@ var $score = $('#score');
 var $time_left = $('#time_left');
 var $questions = $('#questions');
 var $picks = $('#picks');
-// var correct_answer = '';
 // Global Variables
 var trivia = [
     {
-        question: 'What is 1 + 1?', 
-        answers: [3, 2, 11, 0],
+        question: 'What is 3 + 3?', 
+        answers: [33, 9, 8, 6],
         answer() {
             var correct_answer = $(this)[0].answers[1];
-            // var correct_answer = $(this).answers[1];
             return correct_answer;
         }
-        // correct: $(this).answer
     }
+    // {
+    //     question: 'What is 1 + 1?', 
+    //     answers: [3, 2, 11, 0],
+    //     answer() {
+    //         var correct_answer = $(this)[0].answers[1];
+    //         return correct_answer;
+    //     }
+    // }
+    // {
+    //     question: 'What is 5 + 5?', 
+    //     answers: [10, 55, 11, 7],
+    //     answer() {
+    //         var correct_answer = $(this)[0].answers[0];
+    //         return correct_answer;
+    //     }
+    // }
 ];
 console.log(trivia[0].answer());
 var score = 0;
     $score.html('Score: ' + score);
+var wrong = 0;
 var time = 20;
 var time_left;
 var run = function() {
     clearInterval(time_left);
     time_left = setInterval(decrement, 1000);
 }
+
+// generate dynamic trivia
+for (var i = 0; i < trivia.length; i++) {
+    // display question
+    $questions.html(trivia[i].question);
+    // diplay choices
+    // console.log('this works');
+    for (var j = 0; j < trivia[i].answers.length; j++) {
+        var choice = $('<p>').attr(
+            {
+                'class':'col-md-6 choices'
+                , 'answer':trivia[i].answers[j]
+                , 'correct':trivia[i].answer()
+            }
+            ).html(trivia[i].answers[j]);   
+        // // add choice to choices
+        $picks.append(choice);
+        
+    }
+    // console.log($picks);
+    // // click to grab user's answer
+    $('.choices').on('click', function () {
+        console.log($(this).attr('answer')); // this works!!!
+        // check if selected answer is correct
+        if ($(this).attr('answer') === $(this).attr('correct')) {
+            console.log('this is correct!');
+            // add one point to the score
+            score++;
+            // update/display score
+            $score.html('Score: ' + score);
+            // stop timer
+            stop();
+            // go to next question
+            next_question();
+        } else { // if selected answer is wrong
+            console.log('This is wrong because you picked ' + $(this).attr('answer') +' when the correct answer is ' +$(this).attr('correct')+ '!');
+            // add one point to wrong
+            wrong++;
+            console.log(wrong);
+            // stop timer
+            stop();
+            // go to next question
+            next_question();
+        }
+        return
+    });
+}
+
 // Count down time
 function decrement() {
     time--;
@@ -39,40 +101,15 @@ function decrement() {
 function stop() {
     clearInterval(time_left);
 }
-// generate dynamic trivia
-for (var i = 0; i < trivia.length; i++) {
-    // display question
-    $questions.html(trivia[i].question);
-    // diplay choices
-    // console.log('this works');
-    for (var j = 0; j < trivia[i].answers.length; j++) {
-        var choice = $('<p>').attr(
-            {
-                'class':'col-md-6 choices'
-                , 'answer':trivia[i].answers[j]
-                // , 'index':trivia[i].answers.indexOf(trivia[i].answers[j], 0)
-            }
-            ).html(trivia[i].answers[j]);    
-        // add choice to choices
-        $picks.append(choice);
-        // $picks.attr('correct', trivia[i].correct);
-    }
-    console.log($picks);
-    $('#picks').attr({'correct':trivia[i].correct});
-    // // click to grab user's answer
-    // $('.choices').on('click', function () {
-    //     var user_choice = $(this).attr('answer');
-    //     console.log($(this).attr('answer'));
-    //     // console.log('Correct answer: ' + trivia[i].correct);
-    //     console.log(trivia[i].answer());
-    // });
-}
 
-// // click to grab user's answer
-$('.choices').on('click', function () {
-    var user_choice = $(this).attr('answer');
-    console.log($(this).attr('answer'));
-    console.log('Correct answer: ' + trivia[0].answer());
-});
+// change to next question
+function next_question() {
+    // clear question
+    $questions.empty();
+    // clear #picks
+    $picks.empty();
+    return
+
+}
 // start game
 run();
